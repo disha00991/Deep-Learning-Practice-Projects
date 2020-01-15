d4c29db96ac96d9941056844bacbf5d27c52e386
dishasingh <disha@thb.co.in>
2020-01-15 18:16:34 +0530

    Added feature scaling notes

--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
@@ -11,18 +11,22 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer.fit(X[:, 1:3])
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
+transformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [0])],remainder='passthrough')
+X = np.array(transformer.fit_transform(X), dtype=np.float)
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+X
+# feature scaling
+
---
+++ Deep_Learning_A_Z/NOTES.md
@@ -0,0 +1,9 @@
+####Data Science Learning Notes####
+
+How and when to apply feature scaling?
+
+- https://www.geeksforgeeks.org/python-how-and-where-to-apply-feature-scaling/
+- Basically, any algorithms not based on distance won't need it. But the ones which do will.
+- makes algos faster and accurate and prevents any features from dominating.
+- Perfect example : https://www.geeksforgeeks.org/ml-feature-scaling-part-2/
+- Two ways of feature scaling: geeksforgeeks.org/ml-feature-scaling-part-2/
