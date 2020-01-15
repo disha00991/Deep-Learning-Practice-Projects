ddd677c82cd825d71de7414581f0271db1ec7aa2
dishasingh <disha@thb.co.in>
2020-01-16 01:20:26 +0530

    Added logistic regression model

--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
@@ -27,6 +27,16 @@ X = np.array(transformer.fit_transform(X), dtype=np.float)
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
-X
+
+# splitting test and train data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 # feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit(X_train).transform(X_train);
+X_test = sc_X.transform(X_test);
+
+
 
--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -8,4 +8,14 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, 3].values
+
+# splitting test and train data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit(X_train).transform(X_train);
+X_test = sc_X.transform(X_test);
